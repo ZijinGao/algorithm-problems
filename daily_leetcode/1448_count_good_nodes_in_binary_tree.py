08f40558e158709fb04a83c94e0e9ad18c60c065
Zijin Gao <zg235@cornell.edu>
2023-11-28 19:24:14 -0500

    1448 count good nodes in binary tree

---
+++ daily_leetcode/1448_count_good_nodes_in_binary_tree.py
@@ -0,0 +1,22 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        count = 0
+
+        def dfs(node, max_on_path):
+            if not node:
+                return
+            if max_on_path <= node.val:
+                nonlocal count
+                count += 1
+            max_on_path = max(max_on_path, node.val)
+            dfs(node.left, max_on_path)
+            dfs(node.right, max_on_path)
+
+        dfs(root, float('-inf'))
+        return count
