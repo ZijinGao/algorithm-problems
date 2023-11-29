88fbd2419508f0025c7fee919191a7fa670af4a8
Zijin Gao <zg235@cornell.edu>
2023-11-29 16:28:31 -0500

    295 find median from data stream completed

---
+++ daily_leetcode/295_find_median_from_data_stream.py
@@ -0,0 +1,21 @@
+from heapq import heappush, heappop
+class MedianFinder:
+    def __init__(self):
+        self.max_heap = []
+        self.min_heap = []
+
+    def addNum(self, num: int) -> None:
+        heappush(self.max_heap, -num)
+        heappush(self.min_heap, -heappop(self.max_heap))
+
+        if len(self.min_heap) > len(self.max_heap):
+            heappush(self.max_heap, -heappop(self.min_heap))
+
+    def findMedian(self) -> float:
+        return -self.max_heap[0] if (len(self.max_heap) > len(self.min_heap)) else (self.min_heap[0] - self.max_heap[0]) / 2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
