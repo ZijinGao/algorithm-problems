935cbeaaf05dce2d19fdfa4a453bf8daf5956b0e
Zijin Gao <zg235@cornell.edu>
2023-12-28 22:55:43 -0500

    232 implement queue using stacks completed

---
+++ daily_leetcode/232_implement_queue_using_stacks.py
@@ -0,0 +1,25 @@
+# only move the numbers back when needed 
+class MyQueue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        self.stack2.append(x)
+
+    def pop(self) -> int:
+        self._move()
+        ret = self.stack1.pop()
+        return ret
+
+    def peek(self) -> int:
+        self._move()
+        return self.stack1[-1]
+
+    def empty(self) -> bool:
+        return (not self.stack2) and (not self.stack1)
+    
+    def _move(self):
+        if not self.stack1:
+            while self.stack2:
+                self.stack1.append(self.stack2.pop())
