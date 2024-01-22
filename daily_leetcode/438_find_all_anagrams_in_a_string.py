3c7cb85dccb414c24647b6c4037a2727f5ef1bc7
Zijin Gao <zg235@cornell.edu>
2024-01-22 11:16:12 -0500

    438 find all anagrams in a string better way

--- daily_leetcode/438_find_all_anagrams_in_a_string.py
+++ daily_leetcode/438_find_all_anagrams_in_a_string.py
@@ -1,24 +1,17 @@
-from collections import Counter
+from collections import Counter, defaultdict
 class Solution:
     def findAnagrams(self, s: str, p: str) -> list[int]:
-        if len(p) > len(s): return []
         p_table = Counter(p)
-        s_table = Counter(s[:len(p)])
+        s_table = defaultdict(int)
         res = []
-        i = 0
-        j = len(p)
 
-        while j <= len(s):
-            if p_table == s_table:
-                res.append(i)
-            
-            s_table[s[i]] -= 1
-            if s_table[s[i]] < 0:
-                del s_table[s[i]]
-            
-            if j < len(s):
-                s_table[s[j]] += 1
-            
-            i += 1
-            j += 1
+        for i in range(len(s)):
+            s_table[s[i]] += 1
+            if i >= len(p):
+                s_table[s[i-len(p)]] -= 1
+                if s_table[s[i-len(p)]] == 0:
+                    del s_table[s[i-len(p)]]
+            if s_table == p_table:
+                res.append(i-len(p) +1)
+
         return res
