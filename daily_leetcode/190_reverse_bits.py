0c4903022952ee37a2c5f61d7c6a42a7db883cf7
Zijin Gao <zg235@cornell.edu>
2024-02-21 11:57:54 -0500

    add idea and explanation

--- daily_leetcode/190_reverse_bits.py
+++ daily_leetcode/190_reverse_bits.py
@@ -5,3 +5,10 @@ class Solution:
             res = (n & 1) | (res << 1)
             n >>= 1
         return res
+    
+'''
+Problem: Reverse bits of a given 32 bits unsigned integer.
+
+Idea: use (n & 1) to get the last bit of the original number, left shift the result and add the (n & 1) to the result,
+and then right shift n. Until we exhaust the 32 bits.
+'''
