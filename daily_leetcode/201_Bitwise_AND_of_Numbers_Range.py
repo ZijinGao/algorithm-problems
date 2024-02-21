ec624f1de66476cc54d5d012147f0535417bbb53
Zijin Gao <zg235@cornell.edu>
2024-02-21 11:39:37 -0500

    added idea and solution explanation

--- daily_leetcode/201_Bitwise_AND_of_Numbers_Range.py
+++ daily_leetcode/201_Bitwise_AND_of_Numbers_Range.py
@@ -6,3 +6,22 @@ class Solution:
             right >>= 1
             shift += 1
         return left << shift
+    
+
+'''
+Problem: Given two integers left and right that represent the range [left, right], 
+return the bitwise AND of all numbers in this range, inclusive.
+e.g. 1: left = 5, right = 7, returns 4
+e.g. 2: left = 6, right = 7, returns 6
+
+5 in binary representation: 101
+7 in binary representation: 111
+right most same prefix:     ^
+answer:                     100 -> 4
+
+
+6 in binary representation: 110
+7 in binary representation: 111
+right most same prefix:      ^
+answer:                     110 -> 6
+'''
